d71c83fcc6014ed3fb864e26c442958b149c492d
wadonisortiz9628 <31148435+wadonisortiz9628@users.noreply.github.com>
2018-04-29 07:22:55 -0600

    Start-Coming-Soon
    coming soon start

---
+++ calprixComingSoon.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<!-- saved from url=(0035)http://technext.github.io/Imminent/ -->
+<html lang="en-US"><!--<![endif]--><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        
+        <title>
+            Imminent - The most spectacular coming soon template!
+        </title><!-- Behavioral Meta Data -->
+        <meta content="yes" name="apple-mobile-web-app-capable">
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport"><!-- Core Meta Data -->
+        <meta content="ThemeWagon" name="author">
+        <meta content="The most spectacular coming soon template!" name="description">
+        <meta content="Comming Soon, Responsive, Landing Page, One Page" name="keywords"><!-- Open Graph Meta Data -->
+        <meta content="The most spectacular coming soon template!">
+        <meta content="ThemeWagon">
+        <meta content="ThemeWagon">
+        <meta content="website">
+        <meta content="index.html"><!-- Twitter Card Meta Data -->
+        <meta content="summary" name="twitter:card">
+        <meta content="@themewagon" name="twitter:site">
+        <meta content="@themewagon" name="twitter:creator">
+        <meta content="ThemeWagon" name="twitter:title">
+        <meta content="Imminent - The most spectacular coming soon template!" name="twitter:description">
+
+        <link href="http://technext.github.io/Imminent/favicon.png" rel="shortcut icon" type="image/png">
+
+        <!--[if lt IE 9]>
+            <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+        
+        <!-- CSS -->
+        <link href="./Imminent - The most spectacular coming soon template!_files/css" rel="stylesheet" type="text/css"><!-- Styles -->
+        <link href="./Imminent - The most spectacular coming soon template!_files/loader.css" rel="stylesheet" type="text/css">
+        <link href="./Imminent - The most spectacular coming soon template!_files/normalize.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="./Imminent - The most spectacular coming soon template!_files/font-awesome.min.css">
+        <link href="./Imminent - The most spectacular coming soon template!_files/style.css" rel="stylesheet" type="text/css">
+        <!--[if lt IE 9]>
+            <link rel="stylesheet" type="text/css" href="css/ie.css" />
+        <![endif]-->
+        <!-- Javascript -->
+
+        <script src="./Imminent - The most spectacular coming soon template!_files/jquery.js.descarga"></script>
+    </head>
+    <body style="">
+        <div class="preloader" style="display: none;">
+            <div class="loading">
+                <h2>
+                    Loading...
+                </h2>
+                <span class="progress"></span>
+            </div>
+        </div>
+
+        <div class="wrapper">
+            <ul class="scene unselectable" data-friction-x="0.1" data-friction-y="0.1" data-scalar-x="25" data-scalar-y="15" id="scene" style="transform: translate3d(0px, 0px, 0px); transform-style: preserve-3d; backface-visibility: hidden;">
+                <li class="layer" data-depth="0.00" style="position: relative; display: block; left: 0px; top: 0px; transform: translate3d(0px, 0px, 0px); transform-style: preserve-3d; backface-visibility: hidden;">
+                </li>
+                <!-- BG -->
+
+                <li class="layer" data-depth="0.10" style="position: absolute; display: block; left: 0px; top: 0px; transform: translate3d(-15.9887px, -5.03262px, 0px); transform-style: preserve-3d; backface-visibility: hidden;">
+                    <div class="background">
+                    </div>
+                </li>
+
+                <!-- Hero -->
+
+                <li class="layer" data-depth="0.20" style="position: absolute; display: block; left: 0px; top: 0px; transform: translate3d(-31.9774px, -10.0652px, 0px); transform-style: preserve-3d; backface-visibility: hidden;">
+                    <div class="title">
+                        <h2>
+                            CALPRIX
+                        </h2>
+                        <span class="line"></span>
+                    </div>
+                </li>
+
+                <li class="layer" data-depth="0.25" style="position: absolute; display: block; left: 0px; top: 0px; transform: translate3d(-39.9717px, -12.5815px, 0px); transform-style: preserve-3d; backface-visibility: hidden;">
+                    <div class="sphere" style="margin-top: 140.5px;">
+                        <img alt="sphere" src="./Imminent - The most spectacular coming soon template!_files/sphere.png">
+                    </div>
+                </li>
+
+                <li class="layer" data-depth="0.30" style="position: absolute; display: block; left: 0px; top: 0px; transform: translate3d(-47.9661px, -15.0979px, 0px); transform-style: preserve-3d; backface-visibility: hidden;">
+                    <div class="hero" style="margin-top: 179px;">
+                        <h1 id="countdown"><div>228 <span>days</span></div><div>05 <span>hrs</span></div><div>16 <span>min</span></div><div>30 <span>sec</span></div></h1>
+
+                        <p class="sub-title">
+                            Pronto prodras llevarte cualquier articulo de la tienda el mismo dia...!!!<br>
+                            Sin filas y sin retrasos...!!!
+                        </p>
+                    </div>
+                </li>
+                <!-- Flakes -->
+
+                <li class="layer" data-depth="0.40" style="position: absolute; display: block; left: 0px; top: 0px; transform: translate3d(-63.9548px, -20.1305px, 0px); transform-style: preserve-3d; backface-visibility: hidden;">
+                    <div class="depth-1 flake1">
+                        <img alt="flake" src="./Imminent - The most spectacular coming soon template!_files/flakes1.png">
+                    </div>
+
+                    <div class="depth-1 flake2">
+                        <img alt="flake" src="./Imminent - The most spectacular coming soon template!_files/flakes2.png">
+                    </div>
+
+                    <div class="depth-1 flake3">
+                        <img alt="flake" src="./Imminent - The most spectacular coming soon template!_files/flakes3.png">
+                    </div>
+
+                    <div class="depth-1 flake4">
+                        <img alt="flake" src="./Imminent - The most spectacular coming soon template!_files/flakes4.png">
+                    </div>
+                </li>
+
+                <li class="layer" data-depth="0.50" style="position: absolute; display: block; left: 0px; top: 0px; transform: translate3d(-79.9435px, -25.1631px, 0px); transform-style: preserve-3d; backface-visibility: hidden;">
+                    <div class="depth-2 flake1">
+                        <img alt="flake" src="./Imminent - The most spectacular coming soon template!_files/flakes1(1).png">
+                    </div>
+
+                    <div class="depth-2 flake2">
+                        <img alt="flake" src="./Imminent - The most spectacular coming soon template!_files/flakes2(1).png">
+                    </div>
+                </li>
+
+                <li class="layer" data-depth="0.60" style="position: absolute; display: block; left: 0px; top: 0px; transform: translate3d(-95.9321px, -30.1957px, 0px); transform-style: preserve-3d; backface-visibility: hidden;">
+                    <div class="depth-3 flake1">
+                        <img alt="flake" src="./Imminent - The most spectacular coming soon template!_files/flakes1(2).png">
+                    </div>
+
+                    <div class="depth-3 flake2">
+                        <img alt="flake" src="./Imminent - The most spectacular coming soon template!_files/flakes2(2).png">
+                    </div>
+
+                    <div class="depth-3 flake3">
+                        <img alt="flake" src="./Imminent - The most spectacular coming soon template!_files/flakes3(1).png">
+                    </div>
+
+                    <div class="depth-3 flake4">
+                        <img alt="flake" src="./Imminent - The most spectacular coming soon template!_files/flakes4(1).png">
+                    </div>
+                </li>
+
+                <li class="layer" data-depth="0.80" style="position: absolute; display: block; left: 0px; top: 0px; transform: translate3d(-127.91px, -40.261px, 0px); transform-style: preserve-3d; backface-visibility: hidden;">
+                    <div class="depth-4">
+                        <img alt="flake" src="./Imminent - The most spectacular coming soon template!_files/flakes.png">
+                    </div>
+                </li>
+
+                <li class="layer" data-depth="1.00" style="position: absolute; display: block; left: 0px; top: 0px; transform: translate3d(-159.887px, -50.3262px, 0px); transform-style: preserve-3d; backface-visibility: hidden;">
+                    <div class="depth-5">
+                        <img alt="flake" src="./Imminent - The most spectacular coming soon template!_files/flakes(1).png">
+                    </div>
+                </li>
+                <!-- Contact -->
+
+                <li class="layer" data-depth="0.20" style="position: absolute; display: block; left: 0px; top: 0px; transform: translate3d(-31.9774px, -10.0652px, 0px); transform-style: preserve-3d; backface-visibility: hidden;">
+                    <div class="contact">
+                        <ul class="icons">
+                            <li>
+                               <script>
+                                    function doSomething() {
+                                      alert("(849)261-4346 - Walkin Ortiz")
+                                    }
+                                </script>
+                                <a class="dribbble" href="javascript:doSomething();"><i class="fa fa-dribbble"></i></a>
+                            </li>
+
+                            <li>
+                                <script>
+                                    function doSomething() {
+                                      alert("(849)261-4346 - Walkin Ortiz")
+                                    }
+                                </script>
+                                <a class="dribbble" href="javascript:doSomething();"><i class="fa fa-dribbble"></i></a>
+                            </li>
+
+                            <li>
+                                <script>
+                                    function doSomething() {
+                                      alert("(849)261-4346 - Walkin Ortiz")
+                                    }
+                                </script>
+                                <a class="dribbble" href="javascript:doSomething();"><i class="fa fa-dribbble"></i></a>
+                            </li>
+                        </ul>
+                        <a href="https://themewagon.com/" target="_blank">www.calprix.com</a>
+                        <a class="mail" href="mailto:info@example.com?subject=Hi%20ThemeWagon!">calprix@gmail.com</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+
+        <!-- Javascript -->
+        <script src="./Imminent - The most spectacular coming soon template!_files/plugins.js.descarga"></script> 
+        <script src="./Imminent - The most spectacular coming soon template!_files/jquery.countdown.min.js.descarga"></script> 
+        <script src="./Imminent - The most spectacular coming soon template!_files/main.js.descarga"></script>
+
+    
+</body></html>
